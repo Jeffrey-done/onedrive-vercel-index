10e5b32802a214154203cd226d8fc79ded8c5bdd
Jeffrey-done <79053007+Jeffrey-done@users.noreply.github.com>
2021-10-17 14:39:01 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
-let _access_token ='eyJ0eXAiOiJKV1QiLCJub25jZSI6ImxpZjJ1MW55dnlzc3BZbG1QU1RCT1R3VTBLWFVySjItVU4xSGoxS2x2b0UiLCJhbGciOiJSUzI1NiIsIng1dCI6Imwzc1EtNTBjQ0g0eEJWWkxIVEd3blNSNzY4MCIsImtpZCI6Imwzc1EtNTBjQ0g0eEJWWkxIVEd3blNSNzY4MCJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8zMDNiZDcyNC02MjNkLTQ3ZjUtYTU4MC1lZWYxYTBhZDM1ZmQvIiwiaWF0IjoxNjM0NDUxNjUzLCJuYmYiOjE2MzQ0NTE2NTMsImV4cCI6MTYzNDQ1NTU1MywiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFTUUEyLzhUQUFBQXN4MzBBdERCTDdMVngzZlNheC8zSFVhQi9rL05waC9HWjRJeU13TFBhOFU9IiwiYW1yIjpbInB3ZCJdLCJhcHBfZGlzcGxheW5hbWUiOiJmYSIsImFwcGlkIjoiY2FhNmRiMWYtYjc3MS00YTc5LThmZjMtMDFiNmI2YzE0ZjJkIiwiYXBwaWRhY3IiOiIxIiwiZGV2aWNlaWQiOiI1NWJmNTk0Zi04ZGQ1LTRiNjItODY3ZC1kMWVhMjlmZWU4MTIiLCJmYW1pbHlfbmFtZSI6Ik5PIiwiZ2l2ZW5fbmFtZSI6IlMiLCJpZHR5cCI6InVzZXIiLCJpcGFkZHIiOiIxMTYuMjYuMTYuMjAyIiwibmFtZSI6Ik5PIFMiLCJvaWQiOiIxZDMyNzdhOS04MjJmLTRhZTMtODMwOS02ZTA5YjA3MTUyNmEiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDE5ODFDOEM1OSIsInJoIjoiMC5BWEFBSk5jN01EMWk5VWVsZ083eG9LMDFfUl9icHNweHQzbEtqX01CdHJiQlR5MXdBTjQuIiwic2NwIjoiRmlsZXMuUmVhZCBGaWxlcy5SZWFkLkFsbCBwcm9maWxlIG9wZW5pZCBlbWFpbCIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6InpLTmg1WEZKcnFSS0x5amcxeGw4VWtDZ3NLRjliV2RWNHFPZHdnbkMxa1UiLCJ0ZW5hbnRfcmVnaW9uX3Njb3BlIjoiQVMiLCJ0aWQiOiIzMDNiZDcyNC02MjNkLTQ3ZjUtYTU4MC1lZWYxYTBhZDM1ZmQiLCJ1bmlxdWVfbmFtZSI6IlBIT1RPU0hBUkVAdnN3ZWV0LnJlbiIsInVwbiI6IlBIT1RPU0hBUkVAdnN3ZWV0LnJlbiIsInV0aSI6IkZJUWdXZTRuaDAySm81ZHBJWTJ1QUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdLCJ4bXNfc3QiOnsic3ViIjoiZzRSNEgyT204RTRvNTFZdmRqNS1IUXV3V0JqaTlsa1RkNXYySFE1V3M5cyJ9LCJ4bXNfdGNkdCI6MTYyNTk4MjI1NH0.Pfx_R120WdFyVm-3AyKiRLANgyzJ_4X3pmsQW6yvfd9TumMvo72JfgxLQJHzK_yGX7AbvC6RrT24QkwOFgfvMuIq8HwxzxmNSPAimVcS4VHRs9Jp7roDPBeQyh4IrqjiYA-3nQEa_d3I1IkvvxaaLJhOITyjVmlZqJso7XNR5tNQOxzMrYwKNzPuh89WWcuh1P2tIR3ypJj-xkdPK9kwkl3_GUosx4MnBbrl6EzS_VBGxO4U_Iw_c-qGaLpBI9knxGKZoOOGFzIWXcKAqIL3OjNy26subewtIN8C10F2pygvuNbQ68G-V4r4DkcNGQqepKEROcML_zuav7eX_9s6sA
-'
-const getAccessToken = async () => {
-// if (_access_token) {
-// console.log('Fetch token from memory.')
-// return _access_token
-//}
+import Head from 'next/head'
+
+import siteConfig from '../config/site.json'
+import Navbar from '../components/Navbar'
+import FileListing from '../components/FileListing'
+import Footer from '../components/Footer'
+import Breadcrumb from '../components/Breadcrumb'
+
+export default function Home() {
+  return (
+    <div className="dark:bg-gray-900 flex flex-col items-center justify-center min-h-screen bg-white">
+      <Head>
+        <title>{siteConfig.title}</title>
+      </Head>
+
+      <main className="bg-gray-50 dark:bg-gray-800 flex flex-col flex-1 w-full">
+        <Navbar />
+        <div className="w-full max-w-4xl p-4 mx-auto">
+          <Breadcrumb />
+          <FileListing />
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
